520dd2b6a497ab2d61fa288218d9461610356790
Mohit Chotalia <Mohit Chotalia@192.168.0.100>
2023-06-05 12:57:23 +0530

    springboot scheduling added

--- BatchApp01-POC/src/main/java/com/nt/runner/BatchProcessingTest.java
+++ BatchApp01-POC/src/main/java/com/nt/runner/BatchProcessingTest.java
@@ -1,4 +1,4 @@
-//BatchProcessingTestRunner.java
+//BatchProcessingTest.java
 package com.nt.runner;
 
 
@@ -10,24 +10,25 @@ import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BatchProcessingTestRunner implements CommandLineRunner {
+public class BatchProcessingTest {
 	@Autowired
 	private JobLauncher launcher;
 	@Autowired
 	private Job job;
 
-	@Override
-	public void run(String... args) throws Exception {
+	@Scheduled(cron = "${cron.expr}")
+	public void run() throws Exception{
 		//prepare Job Parameters
 		JobParameters params = new JobParametersBuilder().addLong("time", System.currentTimeMillis()).toJobParameters();
 		//run the Job
 		JobExecution execution = launcher.run(job, params);
-		System.out.println("Job Execution Status :: "+execution.getStatus());
+		/*System.out.println("Job Execution Status :: "+execution.getStatus());
 		System.out.println("Job Exit Status :: "+execution.getExitStatus());
-		System.out.println("Job Id :: "+execution.getJobId());
+		System.out.println("Job Id :: "+execution.getJobId());*/
 	}//method
 
 }//class
